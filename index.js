7051d6082b372c5fd15db411692d6f42792def12
Derrick Hammer <derrick@derrickhammer.com>
2022-03-22 11:32:51 -0400

    *Handle browser edge case

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const Gun = require("gun");
 require("gun/nts.js");
 Gun.Rmem = require("gun/lib/rmem.js");
+if (typeof window !== "undefined") {
+  Gun.Rmem = window.Rmem;
+}
 
 module.exports = Gun;
